296c728f8f2585710ff6e249a0de78cd49e73172
harishadepu <harishadepu40@gmail.com>
2026-02-01 12:25:00 +0530

    ff

--- src/components/Todo/TodoApp.jsx
+++ src/components/Todo/TodoApp.jsx
@@ -1,5 +1,5 @@
 import React, { useReducer, useEffect } from 'react';
-import { useLocalStorage } from '../../hooks/useLocalStorage';
+import { useLocalStorage } from '../../hook/useLocalStorage';
 import TaskForm from './TaskForm';
 import TaskList from './TaskList';
 import FilterControls from './FilterControls';
